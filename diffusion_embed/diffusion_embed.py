3137737e9b83b40829b0bc84a0a8e3fc2c463158
Marcel Falkiewicz <mfalkiewicz@cbs.mpg.de>
2017-03-04 13:22:37 +0100

    Re-inserted imports to diffusion_embed

--- diffusion_embed/diffusion_embed.py
+++ diffusion_embed/diffusion_embed.py
@@ -1,3 +1,19 @@
+from __future__ import absolute_import, division, print_function
+
+from scipy.spatial.distance import pdist, squareform
+import scipy.sparse as sps
+from scipy import linalg
+from scipy.sparse.linalg import eigsh, eigs
+
+from nilearn import datasets, plotting
+from nilearn.input_data import NiftiLabelsMasker, NiftiMapsMasker
+from nilearn.connectome import ConnectivityMeasure
+from sklearn.metrics import pairwise_distances
+
+import numpy as np
+
+import h5py
+import os
 
 def get_atlas(name):
     if name == "destrieux_2009":
