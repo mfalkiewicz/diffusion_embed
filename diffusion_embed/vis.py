100f6e6063e58b9a3b3b7af51a1dc6c747d4276f
Nico Scherf <nscherf@googlemail.com>
2017-03-04 21:34:43 +0100

    aligns colormaps so that 0 corresponds to white, to visualize sparsity and sign of affinities...

--- diffusion_embed/vis.py
+++ diffusion_embed/vis.py
@@ -3,7 +3,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plot_matrix(mat, cmap= sns.light_palette((240, 75, 60), input="husl", as_cmap=True)):
+def plot_matrix(mat, cmap =sns.diverging_palette(10, 240, n=15,as_cmap=True)):
     #mask upper triangular part
     mask = np.zeros_like(mat, dtype=np.bool)
     mask[np.triu_indices_from(mask)] = True
@@ -11,11 +11,11 @@ def plot_matrix(mat, cmap= sns.light_palette((240, 75, 60), input="husl", as_cma
     f, ax = plt.subplots(figsize=(fig_w, round(fig_w*1.6)))
     sns.heatmap(mat, mask=mask, cmap = cmap, vmax=.3,
                 square=True, xticklabels=5, yticklabels=5,
-                linewidths=fig_w/1000., cbar_kws={"shrink": .5}, ax=ax)
+                linewidths=fig_w/1000., center=0., cbar_kws={"shrink": .5}, ax=ax)
 
 def plot_node(path, filename):
     f = h5py.File(filename, 'r')
     cmat = f[path+'correlation'][()]
     amat = f[path+'affinity'][()]
-    plot_matrix(cmat, cmap =sns.diverging_palette(240, 10, n=9,as_cmap=True))
+    plot_matrix(cmat)
     plot_matrix(amat)
